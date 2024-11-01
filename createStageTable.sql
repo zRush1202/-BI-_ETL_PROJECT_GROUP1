16b190d87fdbf48b5943b0093ebac22056fc3003
zRush1202 <phutrong.ltv@gmail.com>
2024-11-01 16:52:19 +0700

    script add stage table

---
+++ createStageTable.sql
@@ -0,0 +1,37 @@
+use AirQualityStage;
+
+create table stgAirQuality (
+	stgID int identity(1,1) primary key,
+	stgStateName nvarchar(255),
+	stgCountyName nvarchar(255),
+	stgStateCode int,
+	stgCountyCode int,
+	stgDate datetime,
+	stgAQI int, 
+	stgCategory nvarchar(255) check (stgCategory in ('Good', 'Moderate', 'Unhealthy for Sensitive Groups', 'Unhealthy', 'Very Unhealthy', 'Hazardous')),
+	stgDefiPara nvarchar(255),
+	stgDefiSite nvarchar(255),
+	stgNoSR int, 
+	stgCreated datetime,
+	stgLastUpdated datetime
+);
+
+select* from stgAirQuality;
+
+truncate table stgAirQuality;
+
+create table stgUSCounty (
+	stgUSCID int identity(1,1) primary key,
+	stgCounty nvarchar(255),
+	stgCountyAscii nvarchar(255),
+	stgCountyFull nvarchar(255),
+	stgCountyFips int,
+	stgStateID nvarchar(255),
+	stgStateName nvarchar(255),
+	stgLat float,
+	stgLng float, 
+	stgPopulation bigint
+);
+
+select* from stgUSCounty where stgUSCID = 1;
+select * from stgAirQuality where stgCountyName = 'Los Angeles';
