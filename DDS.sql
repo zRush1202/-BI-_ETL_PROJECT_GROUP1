238f7fb92c1e21a8c5083c4a9f7f257b7b5f9ced
zRush1202 <phutrong.ltv@gmail.com>
2024-12-06 15:49:47 +0700

    NDS to DDS, script, design

---
+++ DDS.sql
@@ -0,0 +1,65 @@
+﻿create database DDS;
+go
+use DDS;
+go
+create table dimState (
+	StateID_SK int primary key,
+	StateName nvarchar(255),
+    StateCode int
+)
+go
+
+create table dimCounty (
+	CountyID_SK int primary key,
+	CountyName nvarchar(255),
+    CountyAscii nvarchar(255),
+    CountyFull nvarchar(255),
+    CountyCode int, 
+    Lat float,
+    Lng float,
+    PopulationCounty int
+)
+go
+
+create table dimTime (
+	TimeID_SK int primary key,
+	Date datetime,
+	Day int,
+	Month int,
+	Quarter int,
+	Year int
+)
+go
+
+create table factAnalysis (
+	StateID_SK int,
+	CountyID_SK int,
+	TimeID_SK int,
+    AQI int,
+    Category nvarchar(255),
+	foreign key (StateID_SK) references dimState (StateID_SK),
+	foreign key (CountyID_SK) references dimCounty (CountyID_SK),
+	foreign key (TimeID_SK) references dimTime (TimeID_SK)
+)
+go
+
+drop table factAnalysis1;
+drop table dimState;
+drop table dimCounty;
+drop table dimTime;
+
+--Phân tích max, min AQI của mỗi state theo mỗi quý trong năm
+select f.StateID_SK, t.Quarter as 'Quarter', max(f.AQI) as 'Max AQI', min(f.AQI) as 'MinAQI'
+from factAnalysis f, dimTime t
+where f.TimeID_SK = t.TimeID_SK
+group by f.StateID_SK, t.Quarter
+order by f.StateID_SK asc
+
+
+--Report the mean and the standard deviation of AQI value for each State during each quarter of years.
+SELECT ds.StateName, dt.Year, dt.Quarter, AVG(fa.AQI) AS MeanAQI, STDEV(fa.AQI) AS StdDevAQI
+FROM factAnalysis fa
+JOIN dimState ds ON fa.StateID_SK = ds.StateID_SK
+JOIN dimTime dt ON fa.TimeID_SK = dt.TimeID_SK
+GROUP BY ds.StateName, dt.Year, dt.Quarter
+ORDER BY ds.StateName, dt.Year, dt.Quarter;
--- metadata.sql
+++ metadata.sql
@@ -13,3 +13,19 @@ insert into DATA_FLOW values ('AirQuality', '2024-11-7', '2021-01-01 16:30')
 insert into DATA_FLOW values ('USCounty', '2024-11-7', '2021-01-01 16:30')
 
 select* from DATA_FLOW;
+
+
+create table DATA_FLOW_NDS (
+    ID int not null identity (1,1),
+    Table_Name varchar(55) not null,
+    CET datetime,
+    LSET datetime,
+    constraint PK_DATA_FLOW_NDS primary key clustered (ID)
+)
+
+truncate table DATA_FLOW_NDS;
+insert into DATA_FLOW_NDS values ('AirQualityNDS', '2021-01-01', '2021-01-01 16:30')
+insert into DATA_FLOW_NDS values ('StateNDS', '2021-01-01', '2021-01-01 16:30')
+insert into DATA_FLOW_NDS values ('CountyNDS', '2021-01-01', '2021-01-01 16:30')
+
+select* from DATA_FLOW_NDS;
